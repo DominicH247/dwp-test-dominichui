b9085178ce7c86ccb54dbc42a3fb498ba695fd9f
Dominic Hui <dominichui112@gmail.com>
2020-03-10 12:21:34 +0000

    added test script to package.json, updated spec.js to use supertest

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -2,5 +2,12 @@ const app = require("../index.js");
 const request = require("supertest");
 
 describe("/api/users", () => {
-  test("GET, responds with status code 200 and an array of user objects", () => {});
+  test("GET, responds with status code 200 and an array of user objects", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body: { users } }) => {
+        expect(users).toBe("array");
+      });
+  });
 });
