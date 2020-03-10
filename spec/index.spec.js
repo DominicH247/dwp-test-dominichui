5f8699a7c79c42ed7976f4a7f79a1380a60119d4
Dominic Hui <dominichui112@gmail.com>
2020-03-10 12:04:11 +0000

    installed supertest as dev dependecy

---
+++ spec/index.spec.js
@@ -0,0 +1,6 @@
+const app = require("../index.js");
+const request = require("supertest");
+
+describe("/api/users", () => {
+  test("GET, responds with status code 200 and an array of user objects", () => {});
+});
