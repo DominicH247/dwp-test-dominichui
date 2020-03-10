433481b5a2fd1b310925663c1e8a51276f291e99
Dominic Hui <dominichui112@gmail.com>
2020-03-10 14:52:19 +0000

    added 404 invalid path error handlers

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ app.use(cors());
 // router
 const apiRouter = require("./routes/apiRouter.js");
 
+// error handlers
+const { handleInvalidPath404 } = require("./errors/index.js");
+
 // route
 app.use("/api", apiRouter);
+
+//controller error handlers
+app.all("/*", handleInvalidPath);
 
 module.exports = app;
