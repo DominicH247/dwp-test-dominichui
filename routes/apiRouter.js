497bf1a72e695ac5f84e6d7fa4b7da74f0a63e13
Dominic Hui <dominichui112@gmail.com>
2020-03-10 11:31:41 +0000

    required in usersRouter

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const apiRouter = express.Router();
 
 // routers
+const usersRouter = require("./usersRouter.js");
 
 // Paths
 apiRouter.use("/users", usersRouter);
