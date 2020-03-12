d7402da911bc0a3b0a215215ce212e2fdcdb0eda
Dominic Hui <dominichui112@gmail.com>
2020-03-12 12:18:17 +0000

    re-wrote test, break down further into atomic units

---
+++ spec/utils.spec.js
@@ -0,0 +1,5 @@
+const { distanceCalc } = require("../utils/utils.js");
+
+describe("distanceCalc(), takes latitude and longitude as parameters, and calculated the distance in miles between the given coordinates and the coordinates for central London", () => {
+  test("Function returns a value");
+});
