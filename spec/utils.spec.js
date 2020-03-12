26bf5ab5ce5d759ec22b33c334a4f543a45f1b36
Dominic Hui <dominichui112@gmail.com>
2020-03-12 12:43:41 +0000

    calcDistance func implementing haversine formula to calculate the distance between 2 sets of coordinates

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,5 +1,10 @@
 const { distanceCalc } = require("../utils/utils.js");
 
 describe("distanceCalc(), takes latitude and longitude as parameters, and calculated the distance in miles between the given coordinates and the coordinates for central London", () => {
-  test("Function returns a value");
+  test("Function returns a value", () => {
+    const lat = 34.003135;
+    const long = -117.7228641;
+    const output = distanceCalc(lat, long);
+    expect(typeof output === "number").toBe(true);
+  });
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,25 @@
-exports.distanceCalc = () => {};
+exports.distanceCalc = (latitude, longitude) => {
+  /* Haversine
+  formula:	a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+  c = 2 ⋅ atan2( √a, √(1−a) )
+  d = R ⋅ c
+  where	φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+  note that angles need to be in radians to pass to trig functions!  */
+
+  // Central London
+  const londonLat = 51.509865;
+  const londonLong = -0.118092;
+
+  // Input
+  const inputLat = latitude;
+  const inputLong = longitude;
+
+  // Radius of earth in meters
+  const R = 6371e3;
+  const phi1 = (londonLat * Math.PI) / 180; // in radians
+  const phi2 = (inputLat * Math.PI) / 180;
+
+  console.log(phi2);
+
+  return 50;
+};
