4b3f9ce053bc59a7b0e3d0dbb30e1db7bbbfdb49
Dominic Hui <dominichui112@gmail.com>
2020-03-13 10:14:35 +0000

    imported in getLondonDistance into test file

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,4 +1,4 @@
-const { distanceCalc } = require("../utils/utils.js");
+const { distanceCalc, getLondonDistance } = require("../utils/utils.js");
 
 describe("distanceCalc(), takes latitude and longitude as parameters, and calculated the distance in miles between the given coordinates and the coordinates for central London", () => {
   test("Function returns a value", () => {
