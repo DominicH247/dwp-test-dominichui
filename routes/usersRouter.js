1c10c8c8bcb7f95ff6bebfac5a6c4c7858dddc33
Dominic Hui <dominichui112@gmail.com>
2020-03-13 15:48:10 +0000

    added handle 405 to both routes and required in

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const usersRouter = express.Router();
 
+// error handlers
+const { handle405InvalidMethods } = require("../errors/index");
+
 // controllers
 const {
   getAllUsers,
   getUsersWithinLondon
 } = require("../controllers/usersController.js");
 
-usersRouter.route("/").get(getAllUsers);
-usersRouter.route("/london").get(getUsersWithinLondon);
+usersRouter
+  .route("/")
+  .get(getAllUsers)
+  .all(handle405InvalidMethods);
+usersRouter
+  .route("/london")
+  .get(getUsersWithinLondon)
+  .all(handle405InvalidMethods);
 
 module.exports = usersRouter;
