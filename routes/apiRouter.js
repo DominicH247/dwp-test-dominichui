25d0266d66d4b7b367ada8376d03093ba42e88b7
Dominic Hui <dominichui112@gmail.com>
2020-03-13 15:43:15 +0000

    added handle405InvalidMethods on all routes

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const apiRouter = express.Router();
 
+// error handlers
+const { handle405InvalidMethods } = require("../errors/index");
+
 // routers
-const usersRouter = require("./usersRouter.js");
+const usersRouter = require("./usersRouter.js").all(handle405InvalidMethods);
 
 // Paths
-apiRouter.use("/users", usersRouter);
+apiRouter.use("/users", usersRouter).all(handle405InvalidMethods);
 
 module.exports = apiRouter;
