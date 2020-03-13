318d48cfbd82f539a3dc34f039935846e632fc9f
Dominic Hui <dominichui112@gmail.com>
2020-03-13 15:42:05 +0000

    added test for inalid methods post,patch,delete

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -27,24 +27,42 @@ describe("/api/users", () => {
 
 describe("/api/users/london", () => {
   test("GET, responds with status code 200 and an array of user object of people living within 50 miles of the city of london", () => {
-    return request.get("/api/users/london").then(({ body: users }) => {
-      console.log(users);
-      expect(typeof users).toBe("object");
-      expect(users.london_users.usersInLondonByCoords[0]).toHaveProperty(
-        "london_distance"
-      );
-      expect(users.london_users.usersListedInLondon[0]).toHaveProperty(
-        "london_distance"
-      );
-      expect(
-        users.london_users.usersInLondonByCoords[0]["london_distance"] <= 50
-      ).toEqual(true);
-      expect(Array.isArray(users.london_users.usersInLondonByCoords)).toBe(
-        true
-      );
-      expect(users.london_users.usersInLondonByCoords.length === 3).toBe(true);
-      expect(Array.isArray(users.london_users.usersListedInLondon)).toBe(true);
-      expect(users.london_users.usersListedInLondon.length === 6).toBe(true);
-    });
+    return request
+      .get("/api/users/london")
+      .expect(200)
+      .then(({ body: users }) => {
+        console.log(users);
+        expect(typeof users).toBe("object");
+        expect(users.london_users.usersInLondonByCoords[0]).toHaveProperty(
+          "london_distance"
+        );
+        expect(users.london_users.usersListedInLondon[0]).toHaveProperty(
+          "london_distance"
+        );
+        expect(
+          users.london_users.usersInLondonByCoords[0]["london_distance"] <= 50
+        ).toEqual(true);
+        expect(Array.isArray(users.london_users.usersInLondonByCoords)).toBe(
+          true
+        );
+        expect(users.london_users.usersInLondonByCoords.length === 3).toBe(
+          true
+        );
+        expect(Array.isArray(users.london_users.usersListedInLondon)).toBe(
+          true
+        );
+        expect(users.london_users.usersListedInLondon.length === 6).toBe(true);
+      });
+  });
+});
+
+describe("Handle invalid methods, POST, PATCH/PUT, DELETE", () => {
+  test("Responds with 405 and message of invalid method", () => {
+    return request
+      .post("/api/users/london")
+      .expect(405)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Method");
+      });
   });
 });
