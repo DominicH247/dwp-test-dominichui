22f935acc7971a715c5aa1807256a63d6ee23bcf
Dominic Hui <dominichui112@gmail.com>
2020-03-13 15:48:39 +0000

    created handle405 error handler responds with 405 status and err message

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,7 @@
 exports.handleInvalidPath404 = (req, res, next) => {
   res.status(404).send({ msg: "Not Found." });
 };
+
+exports.handle405InvalidMethods = (req, res, next) => {
+  res.status(405).send({ msg: "Method Not Allowed" });
+};
